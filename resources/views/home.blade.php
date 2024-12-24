fabc216d4d6aff2820e3fdc9aeb18688fe313b2a
Giordani da Silveira <dani.santossilveira@hotmail.com>
2024-12-24 13:10:40 -0300

    ajuste titulo grafico os por mês

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -188,7 +188,7 @@
             <!-- Area Chart -->
             <div class="card shadow mb-4">
                 <div class="card-header py-3">
-                    <h6 class="m-0 font-weight-bold text-black">Quantidade de Ordens de Serviços por Mês em {{ date('Y') }}</h6>
+                    <h6 class="m-0 font-weight-bold text-black">Quantidade de Ordens de Serviços por Mês em {{ $anoInputValue }}</h6>
                 </div>
                 <div class="card-body">
                     <div class="chart-area">
